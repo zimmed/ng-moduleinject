feea600f8e19376dca2cb8231642395862081297
zimmed <david.zimmelman@qfxgaming.com>
2014-12-11 19:22:58 -0500

    minor change - still not working

--- script/testmain.js
+++ script/testmain.js
@@ -5,56 +5,51 @@
 (function (window, require) {
     "use strict";
 
-$().ready(function () {
-    setTimeout(function () {
-        require.config({
-            paths: {
-                'QUnit': 'lib/qunit-1.16.0',
-                'jQuery': 'lib/jquery-2.1.1'
-            },
-            shim: {
-                'QUnit': {
-                    exports: 'QUnit',
-                    init: function () {
-                        QUnit.config.autostart = false;
-                    }
-                },
-                'jQuery': {
-                    exports: 'jQuery'
+    require.config({
+        paths: {
+            'QUnit': 'lib/qunit-1.16.0',
+            'jQuery': 'lib/jquery-2.1.1'
+        },
+        shim: {
+            'QUnit': {
+                exports: 'QUnit',
+                init: function () {
+                    QUnit.config.autostart = false;
                 }
+            },
+            'jQuery': {
+                exports: 'jQuery'
             }
-        });
+        }
+    });
 
-        // require the unit tests.
-        require(
-            ['jQuery', 'QUnit', 'tests/testsuite', 'tests/main'],
-            function ($, QUnit, TestSuite, tests) {
+    // require the unit tests.
+    require(
+        ['jQuery', 'QUnit', 'tests/testsuite', 'tests/main'],
+        function ($, QUnit, TestSuite, tests) {
 
-                // Create new TestSuite
-                var testSuite = new TestSuite();
-                // Expose child TestSuites
-                testSuite._tests = tests;
-                // Define run method
-                testSuite.run = function () {
-                    this._tests.run();
-                };
-                // Define init function (special for top-level testmain.js)
-                testSuite.init = function () {
-                    // Run QUnit
-                    //QUnit.reset();
-                    //QUnit.start();
-                };
-
-                // Unlike child-levels, this level will not return
-                //  the TestSuite. Instead, it is exposed to the
-                //  window as window.testSuite and trigger loaded
-                //  event.
-
-                window.TestSuite = testSuite;
+            // Create new TestSuite
+            var testSuite = new TestSuite();
+            // Expose child TestSuites
+            testSuite._tests = tests;
+            // Define run method
+            testSuite.run = function () {
+                this._tests.run();
+            };
+            // Define init function (special for top-level testmain.js)
+            testSuite.init = function () {
+                // Run QUnit
                 QUnit.start();
-                $('div#qunitLoaded').trigger("click");
-            }
-        );
-  }, 1000);
-});
+            };
+
+            // Unlike child-levels, this level will not return
+            //  the TestSuite. Instead, it is exposed to the
+            //  window as window.testSuite and trigger loaded
+            //  event.
+
+            window.TestSuite = testSuite;
+            window.TestSuite.runAll();
+        }
+    );
+
 })(window, require);
